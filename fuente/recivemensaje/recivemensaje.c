7bd6a0c3e9f7c6fa594e719fae58710d7cff5045
emilio <mok.boss@hotmail.com>
2018-03-18 17:09:16 -0600

    implementacion comunicacion con fifos

--- fuente/recivemensaje/recivemensaje.c
+++ fuente/recivemensaje/recivemensaje.c
@@ -1,25 +1,49 @@
 #include "fifo.h"
 
-char *fifo = "fifo";
+char *ida = "ida";
+char *vuelta = "vuelta";
+
 int main(void)
 {
-    int descriptorarchivo;
+    int descIda, descVuelta, caracteres_escritos;
     char mensajentrada[TAMEN - 1];
+    char mensajesalida[TAMEN - 1];
+
     /* Abrir fifo para lectura y escritura*/
-    if ((descriptorarchivo = open(fifo, O_RDWR)) < 0)
-        perror("Fallo el open al abrir el fifo");
+    if ((descIda = open(ida, O_RDWR)) < 0)
+        perror("[Receptor]: Fallo el open al abrir el fifo ida");
+    if ((descVuelta = open(vuelta, O_RDWR)) < 0)
+        perror("[Receptor]: Fallo el open al abrir el fifo ida");
+
     /* Recibir mensaje */
     for (;;)
     {
-        if (read(descriptorarchivo, mensajentrada, TAMEN - 1) < 0)
-            perror("Fallo la recepción del mensaje");
-        if (!strcmp(mensajentrada, "Terminar"))
+        if (read(descIda, mensajentrada, TAMEN - 1) < 0)
+            perror("[Receptor]: Fallo la recepción del mensaje");
+        printf("[Receptor]: Mensaje Recibido: %s\n", mensajentrada);
+        if (!strcmp(mensajentrada, "terminar"))
         {
-            printf("Terminando la recepcion de mensajes\n");
+            strcpy(mensajesalida, mensajentrada);
+            if ((caracteres_escritos = write(descVuelta, mensajesalida, TAMEN - 1)) <= 0)
+            {
+                if (!caracteres_escritos)
+                    errno = EAGAIN;
+                perror("[Enviador]:Fallo el envio del mensaje");
+            }
+            printf("[Receptor]: Terminando la recepcion de mensajes\n");
             exit(0);
         }
         else
-            printf("Mensaje Recibido: %s\n", mensajentrada);
+        {
+            strcpy(mensajesalida, mensajentrada);
+            if ((caracteres_escritos = write(descVuelta, mensajesalida, TAMEN - 1)) <= 0)
+            {
+                if (!caracteres_escritos)
+                    errno = EAGAIN;
+                perror("[Enviador]:Fallo el envio del mensaje");
+            }
+        }
+        sleep(1);
     }
     exit(0);
 }
